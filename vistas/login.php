f637fb8fc5cb30fe78f0de9fef1eca7ed4f71c77
knlc01 <43591384+knlc01@users.noreply.github.com>
2021-10-19 12:14:43 -0300

    Add files via upload

---
+++ vistas/login.php
@@ -0,0 +1,19 @@
+
+
+<form action="controladores/ctrlLogin.php" method="POST">
+    <div>
+        <br>
+        <label for="usu">Usuario:</label>
+        <input type="text" name="usu" id="usu" autofocus>
+        
+        <br><br>
+
+        <label for="pass">Contraseña:</label>
+        <input type="password" name="pass" id="pass">
+        
+        <br><br>
+
+        <input type="submit" value="Iniciar sesión">        
+    </div>
+
+</form>
